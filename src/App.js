a8e9200058caff9a05e5350e7d6ff6dd23122227
serjG901 <70537823+serjG901@users.noreply.github.com>
2020-12-18 08:31:13 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,5 +2,5 @@ import React from "react";
 import { BoardConnect } from "./board/connect/BoardConnect";
 
 export default function App() {
-  return <BoardConnect />;
+  return <div className="App"><BoardConnect /></div>;
 }
