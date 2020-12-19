f2c5a4d32c65cdb8e167874aac17def1de7bec67
serjG901 <70537823+serjG901@users.noreply.github.com>
2020-12-19 16:45:34 +0300

    Update TextWithExplane.js

--- src/common/TextWithExplane.js
+++ src/common/TextWithExplane.js
@@ -3,7 +3,7 @@ import React from "react";
 export function TextWithExplane({ explane, union, text }) {
   return (
     <div className="italic font-bold">
-      {explane} {union} {text}
+      {explane} {union} <span className="text-red-500">{text}</span>
     </div>
   );
 }
